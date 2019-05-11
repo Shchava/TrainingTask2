6931d778848d8939aeeec5ca1c65807b6679a274
yaroslav <yarosshc@gmail.com>
2019-05-12 01:34:17 +0300

    add ConnectAbleNetworkPart interface

---
+++ src/main/java/model/ConnectAbleNetworkPart.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.util.List;
+
+public interface ConnectAbleNetworkPart{
+    int getUsedPower();
+    List<ConnectAbleNetworkPart> getConnectedParts();
+    void plugIn(ConnectAbleNetworkPart plugging);
+}
