f3289e42aba3e5a840d40f5ce6a98c4ec726f1b0
yaroslav <yarosshc@gmail.com>
2019-05-11 22:03:24 +0300

    add ElectricityConsumer class

---
+++ src/main/java/model/ElectricityConsumer.java
@@ -0,0 +1,25 @@
+package model;
+
+public class ElectricityConsumer {
+    private String name;
+    private boolean turnedOn;
+    private int power;
+
+    public ElectricityConsumer(String name, int power) {
+        this.name = name;
+        this.power = power;
+        turnedOn = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isTurnedOn() {
+        return turnedOn;
+    }
+
+    public void setTurnedOn(boolean turnedOn) {
+        this.turnedOn = turnedOn;
+    }
+}
