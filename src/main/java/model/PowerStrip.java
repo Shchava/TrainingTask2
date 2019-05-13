7fd1485d067a18577af6bb14614157ac327b6a20
yaroslav <yarosshc@gmail.com>
2019-05-13 13:21:05 +0300

    add PlugIn(ConnectablePart) method

--- src/main/java/model/PowerStrip.java
+++ src/main/java/model/PowerStrip.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PowerStrip {
-    private List<ConnectAbleNetworkPart> sockets;
+    private List<Socket> sockets;
     private final int numberOfSockets;
 
     public PowerStrip(int numberOfSockets){
@@ -15,7 +15,7 @@ public class PowerStrip {
         }
     }
 
-    public List<ConnectAbleNetworkPart> getConnectedParts() {
+    public List<Socket> getConnectedParts() {
         return sockets;
     }
 
@@ -25,5 +25,19 @@ public class PowerStrip {
             power += p.getUsedPower();
         }
         return power;
+    }
+
+    public int getNumberOfSockets() {
+        return numberOfSockets;
+    }
+
+    public void plugIn(ConnectAbleNetworkPart device){
+        for(Socket p:sockets){
+            if(p.getPluggedInDevice() == null){
+                p.plugIn(device);
+                return;
+            }
+        }
+        throw new IllegalStateException("no root to insert new device");
     }
 }
--- src/test/java/testPowerStrip.java
+++ src/test/java/testPowerStrip.java
@@ -28,17 +28,29 @@ public class testPowerStrip {
 
     @Test
     void testGetConnectedParts(){
-        List<ConnectAbleNetworkPart> parts =  testStrip.getConnectedParts();
+        List<Socket> parts =  testStrip.getConnectedParts();
         assertEquals(5,parts.size());
     }
 
     @Test
     void testGetUsedPower(){
-        List<ConnectAbleNetworkPart> parts =  testStrip.getConnectedParts();
+        List<Socket> parts =  testStrip.getConnectedParts();
         ((Socket)parts.get(0)).plugIn(ec1);
         ((Socket)parts.get(1)).plugIn(ec2);
         ((Socket)parts.get(3)).plugIn(ec3);
 
         assertEquals(60,testStrip.getUsedPower());
+    }
+
+    @Test
+    void testPlugIn(){
+        Socket insert1 = new Socket(3);
+        insert1.plugIn(ec1);
+        testStrip.plugIn(insert1);
+        assertEquals(13,testStrip.getUsedPower());
+        Socket insert2 = new Socket(4);
+        insert2.plugIn(ec2);
+        testStrip.plugIn(insert2);
+        assertEquals(37,testStrip.getUsedPower());
     }
 }
